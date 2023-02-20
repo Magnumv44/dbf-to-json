03455469d157156684d2d49fcb58cd9128d7cb0e
Magnumv44 <vitaliy1984@gmail.com>
2023-02-20 15:42:33 +0200

    Завантаження коду проекту

---
+++ DbfToJson/DbfToJson/DbfToJson/DbfJsonStructure.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFtoJson
+{
+    /// <summary>
+    /// Клас для опису структури майбутьнього JSON файлу.
+    /// </summary>
+    internal class DbfJsonStructure
+    {
+        /// <summary>
+        /// Унікальний номер листа непрацездатності
+        /// </summary>
+        public string WIC_NUM { get; set; }
+
+        /// <summary>
+        /// Загальна кількість днів, що підлягаюсть оплаті
+        /// </summary>
+        public int WIC_NUMBER_ALL { get; set; }
+
+        /// <summary>
+        /// Кількість днів, зо підлягають оплаті за рахунок коштів фонду
+        /// </summary>
+        public int WIC_NUMBER_PFU { get; set; }
+
+        /// <summary>
+        /// Загальна сума з копійками (в гривнях)
+        /// </summary>
+        public decimal WIC_SUMM_ALL { get; set; }
+
+        /// <summary>
+        /// Сума коштів за рахунок фонду (в гривнях)
+        /// </summary>
+        public decimal WIC_SUMM_PFU { get; set; }
+
+        /// <summary>
+        /// Кількість днів, що підлягають оплаті за рахунок фонду постраждалим в наслідок аваріі на ЧАЕС
+        /// </summary>
+        public int WIC_NUMBER_CHAES { get; set; }
+
+        /// <summary>
+        /// Сума коштів за рахунок фонду постраждалим в наслідок аваріі на ЧАЕС
+        /// </summary>
+        public decimal WIC_SUMM_CHAES { get; set; }
+    }
+}
---
+++ DbfToJson/DbfToJson/DbfToJson/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using dBASE.NET;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+
+namespace DBFtoJson
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var dbf = new Dbf();
+            dbf.Read(@"D:\temp\C101T1.DBF");
+
+            // TODO Сделать выбор файла с последующей передачей его в dbf.Read
+            //string[] dbfFile = Directory.GetFiles(@"D:\temp\", "*.dbf");
+
+            DbfJsonStructure person = new DbfJsonStructure();
+
+            StreamWriter file = File.CreateText(@"D:\temp\person.json");
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            // Створюємо массив данніх з підтримкою різних форматів даних
+            JsonArray test = new JsonArray();
+
+            // Додаємо виключно ті дані, які нам потрібні
+            foreach (DbfRecord record in dbf.Records)
+            {
+                person.WIC_NUM = record[11].ToString();
+                person.WIC_NUMBER_ALL = Int32.Parse(record[16].ToString());
+                person.WIC_NUMBER_PFU = Int32.Parse(record[17].ToString());
+                person.WIC_SUMM_ALL = Decimal.Parse(record[18].ToString());
+                person.WIC_SUMM_PFU = Decimal.Parse(record[19].ToString());
+
+                test.Add(JsonSerializer.SerializeToNode(person, options)); // Додаємо серіалізований об'єкт до массиву
+            }
+
+            file.WriteLine(test);
+            file.Close();
+        }
+    }
+}
