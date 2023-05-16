39f27cd822a8b909bc540f37c63ae255583d01b5
Magnumv44 <vitaliy1984@gmail.com>
2023-05-16 21:01:45 +0300

    Предварительная версия

--- DbfToJson/DbfToJson/DbfToJson/Program.cs
+++ DbfToJson/DbfToJson/DbfToJson/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using dBASE.NET;
 using System.Text.Json;
@@ -11,38 +12,76 @@ namespace DBFtoJson
     {
         static void Main()
         {
-            var dbf = new Dbf();
-            dbf.Read(@"D:\temp\C101T1.DBF");
+            // определим пути к папкам в отдельные переменные
+            string incomingDataFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "incoming");
+            string outDataFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "out");
 
-            // TODO Сделать выбор файла с последующей передачей его в dbf.Read
-            //string[] dbfFile = Directory.GetFiles(@"D:\temp\", "*.dbf");
-
-            DbfJsonStructure person = new DbfJsonStructure();
-
-            StreamWriter file = File.CreateText(@"D:\temp\person.json");
-
-            JsonSerializerOptions options = new JsonSerializerOptions()
+            // проверим, есть ли папка incoming, и если нет - создадим ее
+            if (!Directory.Exists(incomingDataFolder))
             {
-                WriteIndented = true
-            };
+                Directory.CreateDirectory(incomingDataFolder);
+                Console.WriteLine("The 'incoming' directory was created");
+                Console.ReadKey();
+            }
 
-            // Створюємо массив данніх з підтримкою різних форматів даних
-            JsonArray test = new JsonArray();
+            // определим путь к файлу базы данных
+            string dbfPath = Path.Combine(incomingDataFolder, "C101T1.DBF");
 
-            // Додаємо виключно ті дані, які нам потрібні
+            // проверим, существует ли файл базы данных, и если нет - выведем сообщение об ошибке и завершим работу программы
+            if (!File.Exists(dbfPath))
+            {
+                Console.WriteLine($"The '{dbfPath}' file does not exist.");
+                Console.WriteLine("Please enter the path to the DBF file:");
+                dbfPath = Console.ReadLine();
+            }
+
+            // загрузим базу данных
+            var dbf = new Dbf();
+            dbf.Read(dbfPath);
+
+            // создадим список объектов DbfJsonStructure
+            var persons = new List<DbfJsonStructure>();
+
+            // преобразуем каждую запись из базы данных в объект DbfJsonStructure и добавим его в список
             foreach (DbfRecord record in dbf.Records)
             {
+                var person = new DbfJsonStructure();
                 person.WIC_NUM = record[11].ToString();
                 person.WIC_NUMBER_ALL = Int32.Parse(record[16].ToString());
                 person.WIC_NUMBER_PFU = Int32.Parse(record[17].ToString());
                 person.WIC_SUMM_ALL = Decimal.Parse(record[18].ToString());
                 person.WIC_SUMM_PFU = Decimal.Parse(record[19].ToString());
-
-                test.Add(JsonSerializer.SerializeToNode(person, options)); // Додаємо серіалізований об'єкт до массиву
+                persons.Add(person);
             }
 
-            file.WriteLine(test);
-            file.Close();
+            // создадим объект JsonArray, содержащий сериализованные объекты DbfJsonStructure
+            JsonArray jsonArray = new JsonArray();
+            JsonSerializerOptions jsonOptions = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            foreach (DbfJsonStructure person in persons)
+            {
+                JsonNode jsonNode = JsonSerializer.SerializeToNode(person, jsonOptions);
+                jsonArray.Add(jsonNode);
+            }
+
+            // создадим выходную папку, если она не существует
+            if (!Directory.Exists(outDataFolder))
+            {
+                Directory.CreateDirectory(outDataFolder);
+                Console.WriteLine("The 'out' directory was created");
+            }
+
+            // создадим имя выходного файла, основанное на текущей дате и времени
+            string outputFileName = $"person_{DateTime.Now.ToString("yyyyMMdd_HHmmss")}.json";
+            string outputPath = Path.Combine(outDataFolder, outputFileName);
+
+            // создадим выходной файл и запишем в него массив JsonArray
+            using (StreamWriter outputFile = new StreamWriter(outputPath))
+            {
+                outputFile.WriteLine(jsonArray.ToString());
+            }
         }
     }
 }
